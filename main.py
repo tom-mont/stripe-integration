e4b741da7d529b1783dbeb75e1746a4907b9fc67
Tom Montgomery <tom.b.montgomery@gmail.com>
2024-03-30 07:28:48 +0200

    stripe API key using environment variables

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 # This script prints "Hello world"
 import stripe
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 
 def main():
-    stripe.api_key = "sk_test_51Oxmi7P6DnIdemxySI00JjO422966sjJJjTKJTpvUA5OJPiNAwVGyFlN8gehfasFdp34HJgHcWTGj5257SNSIlLx009H88is3v"
-    stripe.Customer.create(
-        name="Jenny Rosen",
-        email="jennyrosen@example.com",
-    )
-    print("Hello, world!")
+    # stripe.api_key = "sk_test_51Oxmi7P6DnIdemxySI00JjO422966sjJJjTKJTpvUA5OJPiNAwVGyFlN8gehfasFdp34HJgHcWTGj5257SNSIlLx009H88is3v"
+    # stripe.Customer.create(
+    # name="Jenny Rosen",
+    # email="jennyrosen@example.com",
+    # )
+    # stripe.api_key = os.environ["STRIPE_API_KEY"]
+    print(os.getenv("STRIPE_API_KEY"))
 
 
 if __name__ == "__main__":
